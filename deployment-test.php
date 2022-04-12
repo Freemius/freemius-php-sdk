428ab5026e67beca07412f60b07a7c39efbaa14b
Vova Feldman <vova.feldman@gmail.com>
2022-04-12 11:56:46 +0300

    Enriched the example to showcase how to download a deployed version.

--- deployment-test.php
+++ deployment-test.php
@@ -10,10 +10,20 @@
     // Init SDK.
     $api = new Freemius_Api(FS__API_SCOPE, FS__API_DEV_ID, FS__API_PUBLIC_KEY, FS__API_SECRET_KEY);
 
-    $result = $api->Api('plugins/115/tags.json', 'POST', array(
+    // Deploy new version.
+    $tag = $api->Api('plugins/115/tags.json', 'POST', array(
         'add_contributor' => true
     ), array(
         'file' => 'C:\xampp\htdocs\deployment-via-sdk\my-plugin.zip'
     ));
 
-    print_r($result);
+    // Generate secure download URLs.
+    $free_version_download_url = $api->GetSignedUrl( "/plugins/{$tag->plugin_id}/tags/{$tag->id}.zip?is_premium=false" );
+    $paid_version_download_url = $api->GetSignedUrl( "/plugins/{$tag->plugin_id}/tags/{$tag->id}.zip?is_premium=true" );
+
+    // Download the paid version.
+    if ( file_put_contents($local_file_path, file_get_contents($paid_version_download_url)) ) {
+        // Successfully downloaded and stored.
+    }
+    
+    print_r($tag);

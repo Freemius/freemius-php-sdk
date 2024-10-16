2b3f7cba516b10b16a4411c120bebfc5c8bcab52
Swashata Ghosh <swas@freemius.com>
2024-10-16 12:43:16 +0530

    [documentation] [license] [minor] Minor comment update + Moved license into the root.

--- tests/credentials.example.php
+++ tests/credentials.example.php
@@ -1,9 +1,9 @@
 <?php
     // Rename this file to `credentials.php` and fill in your actual credentials.
 
-    define('FS_API__ADDRESS', 'http://api.example.com:8080'); // e.g., 'https://api.freemius.com'
+    define('FS_API__ADDRESS', 'https://sandbox-api.freemius.com'); // The Freemius API server URL, e.g., 'https://api.freemius.com'
     define('FS__API_SCOPE', 'plugin'); // e.g., 'plugin', 'store', 'developer'
     define('FS__API_ENTITY_ID', 0); // The ID of the scope entity
-    define('FS__API_PUBLIC_KEY', 'pk_YOUR_PUBLIC_KEY'); // Your public key
-    define('FS__API_SECRET_KEY', 'sk_YOUR_SECRET_KEY'); // Your secret key
+    define('FS__API_PUBLIC_KEY', 'pk_YOUR_PUBLIC_KEY'); // The public key of the scope entity
+    define('FS__API_SECRET_KEY', 'sk_YOUR_SECRET_KEY'); // The secret key of the scope entity
     define('FS__TEST_VALID_LICENSE_KEY', 'sk_YOUR_LICENSE_KEY');
